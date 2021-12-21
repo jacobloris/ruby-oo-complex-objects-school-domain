4ba8d8af24e2f9ebd596f0633cde2c7ef2263462
jacobloris <jacobloris@outlook.com>
2021-12-20 18:47:26 -0700

    commit tree 8085bd13f9c4f979bdc84573ad851cf6bee7886d

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
 # code here!
+class School
+    attr_accessor :roster
+    attr_reader :name
+    def initialize(name)
+      @name = name
+      @roster = {}
+    end
+    def add_student(name, grade)
+      @roster[grade] ?  @roster[grade] << name : @roster[grade] = [name]
+    end
+    def grade(grade)
+      @roster[grade]
+    end
+    def sort()
+      sorted_roster = {}
+      @roster.sort.each {|grade, roster| sorted_roster[grade] = roster.sort}
+      sorted_roster
+    end
+  end
